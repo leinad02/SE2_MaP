32523781663b2862a4d99e2309887901a389ec90
mkloss94 <m1kloss@edu.aau.at>
2017-04-03 09:54:59 +0200

    LibGDX upload

---
+++ android/src/at/michi/map/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package at.michi.map;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import at.michi.map.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
