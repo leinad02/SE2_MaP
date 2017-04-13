cd3ecee3d103e28ea3c5c4d61296dede650981c5
mkloss94 <m1kloss@edu.aau.at>
2017-04-13 17:08:10 +0200

    GameConstants Klasse erstellt: dient für den grundlegenden Aufbau der Screens. (Hilfe für die jeweilige Breite und Höhe der jeweiligen Devices.

---
+++ core/src/at/michi/map/GameConstants.java
@@ -0,0 +1,17 @@
+package at.michi.map;
+
+import com.badlogic.gdx.Gdx;
+
+/**
+ * Created by Michi on 06.04.17.
+ */
+public class GameConstants {
+    public static final String skin = "skin/glassy-ui.json";
+    public static final String skinAtlas = "skin/glassy-ui.atlas";
+    public static final int screenWidth = Gdx.graphics.getWidth();
+    public static final int screenHeight = Gdx.graphics.getHeight();
+    public static final int centerX = screenWidth/2;
+    public static final int centerY = screenHeight/2;
+    public static final int col_width = screenWidth/8;
+    public static final int row_height = screenHeight/8;
+}
