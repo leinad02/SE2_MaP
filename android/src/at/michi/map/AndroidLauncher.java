e4d1d7aa24a718fe30971f39e43f27366b355d07
mkloss94 <m1kloss@edu.aau.at>
2017-04-16 16:46:03 +0200

    Kleine Änderung in AndroidLauncher: Damit der Autosleep Modus von den Smartphones deaktiviert wird, während die App läuft.

--- android/src/at/michi/map/AndroidLauncher.java
+++ android/src/at/michi/map/AndroidLauncher.java
@@ -1,6 +1,7 @@
 package at.michi.map;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
@@ -11,6 +12,8 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		//Damit der Screen immer aktiv bleibt, diese kleine Änderung
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 		initialize(new Main(), config);
 	}
 }
