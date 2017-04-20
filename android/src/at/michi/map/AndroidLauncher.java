1611095bfe12fb23e2489fb04d1bb9adea8b1592
SaW2017 <littlemiss0085@yahoo.de>
2017-04-20 13:35:48 +0200

    ich habe versucht nochmal ein reines Programm, welches nur die Animation darstellen soll zu starten, stoße aber weiterhin auf Probleme :-/

--- android/src/at/michi/map/AndroidLauncher.java
+++ android/src/at/michi/map/AndroidLauncher.java
@@ -2,15 +2,29 @@ package at.michi.map;
 
 import android.os.Bundle;
 
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+
 import at.michi.map.MyGdxGame;
 
 public class AndroidLauncher extends AndroidApplication {
+
+
+
+
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new MyGdxGame(), config);
 	}
+
+
 }
--- core/src/at/michi/map/MyGdxGame.java
+++ core/src/at/michi/map/MyGdxGame.java
@@ -4,30 +4,52 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+
+	private SpriteBatch sBatch;
+	private TextureAtlas portalAtlas;
+	//um Animationsdaten zu speichern, welche Bilder und wie lange sie gezeigt werden sollen etc.
+	private Animation animation;
+	//Zeitpunkt für den Start der Animation
+	private float timePassed = 0;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		//img = new Texture("badlogic.jpg");
+		sBatch = new SpriteBatch();
+		portalAtlas = new TextureAtlas(Gdx.files.internal("Portal.atlas"));
+		//angeben wie viele und was für Bilder in einer Sekunde gezeigt werden sollen und wo "getRegions()"
+		animation = new Animation(1/30f,portalAtlas.getRegions());
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		/*Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
-		batch.end();
+		batch.end();*/
+		Gdx.gl.glClearColor(1,1,1,1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		sBatch.begin();
+		timePassed += Gdx.graphics.getDeltaTime();
+		sBatch.draw((Texture) animation.getKeyFrame(timePassed, true),300, 0);
+		sBatch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		//img.dispose();
+		sBatch.dispose();
+		portalAtlas.dispose();
 	}
 }
